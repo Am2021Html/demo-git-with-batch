27ebe92d5f741e0afd435224d782f5e72e4ec038
amirhosein shahrstani <93426971+Am2021Html@users.noreply.github.com>
2021-11-28 21:51:01 +0330

    Update and rename README.md to Ajiket.bat

---
+++ Ajiket.bat
@@ -0,0 +1,36 @@
+@echo off
+title Ajiket --2021 
+echo Hello Update soft source and unistal bug
+pause
+echo [Y]
+pause
+ipconfig 
+pause
+ping Youtube.com
+pause
+echo update
+pause 
+data =['help']
+data ==[$ Ajiket ]
+pause
+*(---jiket)
+folder{
+   & help &
+   Update ==={
+    $Jiket 
+	 Border@{
+	   $ jiket --Git 
+	   &داده[data == ping '$ http://www.google.ca/#'
+	 }
+   }
+ }
+ pause 
+  data =={
+        $ewaduiscz9x0-gisdgxvcopkpt90tgdpkvx9e48tgr-0sdiophtjlk
+		rdf09fdsfvxc9080349-09er0-9fdfopdir4owiresfp]trtdgofijewads
+		dfusoigoeitre9gif9dgoigopemvrewor5390589048230943049wresdfop
+		9sdf8098z9ifte4ke54t90et9sd8dsufioru094tsfd9ie4w3i2190384564390
+		} $data 
+  }
+@echo off 
+البته تا حد زیادی شوخی است 
